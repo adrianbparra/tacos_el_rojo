2265a35a71a68107634fb56fa359eb717eed2ec9
adrianbparra <adrianplane12@gmail.com>
2021-05-18 21:59:38 -0700

    Update Navbar
    Update navbar to start new ux design.

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,53 +1,45 @@
-import React,{useEffect, useState} from "react"
+import React from "react"
 import {Link} from "react-router-dom";
-
+import tacosElRojoLogo from "../assets/images/tacos_el_rojo_logo_2.png";
 
 
 
 function NavBar() {
 
-    const [topDistance, setTopDistance]= useState(0)
-
-    useEffect(()=>{
-        
-        window.addEventListener("scroll", NavScroll)
-
-        return function cleanup(){
-            window.removeEventListener("scroll", NavScroll)
-        }
-    },[])
-
-    function NavScroll (e){
-
-        // console.log(window.scrollY)
-        setTopDistance(window.scrollY)
-    }
-    
     
     return(
         
-        <nav className={`${topDistance >= 100 && "bg-orange-100"} bg-opacity-75 w-full container pt-1 sm:pt-3 pb-3 fixed px-2 md:px-10 text-md md:text-2xl md:text-3xl z-50 text-red-700`}>
-            <ul className="flex justify-between flex-wrap ">
-                <li className=" w-full text-center md:flex-grow md:text-left md:w-auto">
-                    <Link to="/">
-                        <span>Tacos</span>
-                        <span className="font-lobster">El Rojo</span>
-                    </Link>
-                </li>
-                <li className="md:px-2">
-                    <Link to="/menu" className="hover:text-black focus:text-black">MENU</Link>
+        <nav className="font-lobster bg-opacity-75 w-full container flex flex-row py-5 px-2 text-md md:text-2xl md:text-3xl text-red-700">
+
+            <span className="border-solid border-black border-t-2 border-b-2 h-29 w-4 block"/>
+
+            <ul className="flex-grow flex justify-around items-center border-solid border-black border-t-2 border-b-2">
+
+                <li className="md:px-2 flex-1 text-center">
+                    <Link to="/menu" className="hover:text-1xl md:hover:text-4xl focus:text-black">MENU</Link>
                     
                 </li>
-                <li className="md:px-2">
-                    <Link to="/contact" className="hover:text-black focus:text-black">CONTACT</Link>
+                <li className="md:px-2 flex-1 text-center">
+                    <Link to="/events" className="hover:text-1xl md:hover:text-4xl focus:text-black py-3">EVENTS</Link>
                 </li>
-                <li className="md:px-2">
-                    <Link to="/events" className="hover:text-black focus:text-black">EVENTS</Link>
+                <li className="md:px-2 flex-1">
+                    <Link to="/" className="flex justify-around">
+                        <img 
+                            className="h-24 w-24" 
+                            src={tacosElRojoLogo}
+                            alt="Tacos El Rojo Logo"
+                        />
+                        
+                    </Link>
                 </li>
-                <li className="md:px-2">
-                    <Link to="/merch" className="hover:text-black focus:text-black">MERCH</Link>
+                <li className="md:px-2 flex-1 text-center">
+                    <Link to="/merch" className="hover:text-2xl md:hover:text-4xl focus:text-black">MERCH</Link>
+                </li>
+                <li className="md:px-2 flex-1 text-center">
+                    <Link to="/catering" className="hover:text-2xl md:hover:text-4xl focus:text-black">CATERING</Link>
                 </li>
             </ul>
+            <span className="border-solid border-black border-t-2 border-b-2 h-29 w-4 block"/>
         </nav>
     )
 
