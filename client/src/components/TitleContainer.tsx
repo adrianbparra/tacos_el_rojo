ba9034d1b4dd8b25ddd40528d315a5bf0b1ac3ff
adrianbparra <adrianplane12@gmail.com>
2021-12-15 19:29:45 -0700

    Update TitleContainer.tsx
    changed title container to update color on hover.

--- client/src/components/TitleContainer.tsx
+++ client/src/components/TitleContainer.tsx
@@ -13,15 +13,15 @@ const TitleContainer:FC<ILinkProps> = (props) => {
     const {title, children} = props;
 
     return (
-        <Link to={title}>
-        <div className="relative">
-            <img src={HeaderBanner} alt="Banner" className="mx-auto h-12 md:h-24"/>
-            <h2 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-lobster text-2xl md:text-6xl text-red-700">
-                {children}
-            </h2>
+        <div className="relative inline-block text-red-700 hover:text-black">
+            <Link to={title}>
+                <img src={HeaderBanner} alt={title + " banner"} className="mx-auto h-12 md:h-24"/>
+                <h2 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-lobster text-2xl md:text-6xl">
+                    {children}
+                </h2>
+            </Link>
         </div>
-        </Link>
-            )
+        )
 }
 
 export default TitleContainer
