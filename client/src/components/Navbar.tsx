08b36949e5ff35e72ab6939801e939ca24a709a2
adrianbparra <adrianplane12@gmail.com>
2021-12-23 19:48:09 -0700

    Update Navbar.tsx
    made logo show on mobile and desktop

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -4,9 +4,7 @@ import tacosElRojoLogo from "../assets/images/tacos_el_rojo_logo_2.png";
 
 import NavLink from "./NavLink";
 
-
 const NavBar : FC = () => {
-
     
     return(
         
@@ -16,20 +14,20 @@ const NavBar : FC = () => {
                 <path d="M28 121C7.75 124 1 132 0 141V94.5V63V32.5V15C1 5 9.75 1 28 1" stroke="black" stroke-width="3"/>
             </svg>
 
-            <div className="flex-grow flex justify-around items-center border-solid border-black border-t-2 border-b-2 mb-3">
+            <div className="flex-grow flex justify-around items-center border-solid border-black border-t-2 border-b-2 mb-3" >
 
                 <NavLink to="/menu">
-                    MENU
+                    Menu
                 </NavLink>
 
-                <NavLink to="/events">
-                    EVENTS
+                <NavLink to="/Contact">
+                    Contact
                 </NavLink>
 
-                <li className="md:px-2 flex-1 hidden lg:block">
+                <li className="md:px-2 flex-1 block">
                     <Link to="/" className="flex justify-around">
                         <img 
-                            className="h-24 w-24" 
+                            className="h-20 w-20 md:h-24 md:w-24" 
                             src={tacosElRojoLogo}
                             alt="Tacos El Rojo Logo"
                             loading="lazy"
@@ -38,18 +36,21 @@ const NavBar : FC = () => {
                     </Link>
                 </li>
 
-                <NavLink to="/merch">
-                    MERCH
+                <NavLink to="/events">
+                    Events
                 </NavLink>
 
-                <NavLink to="/contact">
-                    CONTACT
+                <NavLink to="/merch">
+                    Merch
                 </NavLink>
+
             </div>
             
-            <svg width="100%" className="w-4" viewBox="0 0 16 141" fill="none" xmlns="http://www.w3.org/2000/svg">
+            
+            <svg className="w-4" width="100%" viewBox="0 0 16 141" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M-6 122C8.75 128 17.5 132 17.5 141V94.5V63V32.5V15C17.5 6 9.75 1 -8 1" stroke="black" stroke-width="3"/>
             </svg>
+
 
         </nav>
     )
