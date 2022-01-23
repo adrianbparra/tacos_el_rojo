f62ff0b9e08a0a6c850263b7caf927b0a6e9c092
adrianbparra <adrianplane12@gmail.com>
2022-01-22 18:31:23 -0700

    Updated Home
    Added hours and updated address.

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,11 +1,14 @@
 import React from "react"
+import moment from "moment";
 import {FaFacebookSquare,FaInstagramSquare} from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 import TitleContainer from "./TitleContainer";
 import HomeContainer from "./HomeContainer";
 
 import shirt from "../assets/images/merch_shirt.jpg";
 import MenuItem from "./MenuItem";
+
 
 
 function Home (){
@@ -18,12 +21,9 @@ function Home (){
                     Menu
                 </TitleContainer>
                 
-                {/* <p className="lg:text-2xl">
-                    I offer a selection of tacos, burritos and quesadillas. With a range of different meats to choose from to top the main dish. Includes savory condiments to make our food favlorful.
-                </p> */}
                 <div className="my-14 grid justify-items-center gap-y-16 md:gap-y-14 xl:gap-y-24 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
                     {
-                        [{to:"menu",img:"tacos",name:"tacos"},{to:"menu",img:"quesadilla",name:"quesadilla"},{to:"menu",img:"tacos",name:"tacos"},{to:"menu",img:"quesadilla",name:"quesadilla"}].map( menuItem => {
+                        [{to:"menu",img:"tacos",name:"tacos"},{to:"menu",img:"quesadilla",name:"quesadilla"},{to:"menu",img:"tacos",name:"tacos"},{to:"menu",img:"quesadilla",name:"hotdogs"}].map( menuItem => {
                             return <MenuItem to={menuItem.to} img={menuItem.img} name={menuItem.name} />
                         })
                     }
@@ -35,19 +35,54 @@ function Home (){
                 <TitleContainer title="contact">
                     Contact
                 </TitleContainer>
+                
+                <div className="text-2xl grid gap-y-8 my-2">
+                    <address className="not-italic flex justify-center"> 
+                        <a className="hover:text-red-600" href="https://goo.gl/maps/MbYSSaSbpZkYaHGo6" target="_blank" rel="noopener noreferrer">
+                            <p>3000 W McDowell Rd</p>
+                            <p>Phoenix, AZ 85009</p>
+                        </a>
+                    </address>
 
-                <p>Give us a call at:</p>
-                <a className="text-xl font-bold hover:text-red-600" href="tel:4808229166">(480)822-9166</a>
-                <p>Or Follow us:</p>
-                <div className="flex justify-center text-xl">
-                    <span>Tacos</span>
-                    <a className="px-1 hover:text-red-600 text-3xl" href="https://www.instagram.com/tacos_el_rojo_/" target="_blank" rel="noopener noreferrer"><FaInstagramSquare/></a>
-                    <a className="px-1 hover:text-red-600 text-3xl" href="https://www.facebook.com/tacos.e.rojo" target="_blank" rel="noopener noreferrer"><FaFacebookSquare/></a>
+                    <div className="text-xl mx-6 xl:mx-48 xl:text-2xl">
+                    {
+                        [{id:1, "dayOfWeek":"Monday", "openTime": 0, "closeTime": 0 },
+                        {id:2, "dayOfWeek":"Tuesday", "openTime": 0, "closeTime": 0 },
+                        {id:2, "dayOfWeek":"Wednesday", "openTime": 0, "closeTime": 0 },
+                        {id:2, "dayOfWeek":"Thursday", "openTime": 17, "closeTime": 22 },
+                        {id:2, "dayOfWeek":"Friday", "openTime": 17, "closeTime": 22 },
+                        {id:2, "dayOfWeek":"Saturday", "openTime": 17, "closeTime": 22 },
+                        {id:2, "dayOfWeek":"Sunday", "openTime": 0, "closeTime": 0 },].map(day => 
+                            {
+                                const {dayOfWeek, openTime, closeTime} = day;
+                                return  <div className="grid grid-cols-2 ">
+                                            <p>{dayOfWeek}</p>
+                                            
+                                            <p>
+                                            {
+                                                openTime < closeTime ? moment(openTime,"HH").format("LT") + " - "+ moment(closeTime,"HH").format("LT") : "Closed"
+                                            }
+                                            </p>
+                                        </div>
+
+                            })
+                        
+                    }
+                    </div>
+
+                    <div>
+
+                        <a className="hover:text-red-600" href="tel:4808229166">(480)822-9166</a>
+                    </div>
+        
+                    <div className="flex justify-center">
+                        <a className="px-1 hover:text-red-600 text-5xl" href="https://www.instagram.com/tacos_el_rojo_/" target="_blank" rel="noopener noreferrer"><FaInstagramSquare/></a>
+                        <a className="px-1 hover:text-red-600 text-5xl" href="https://www.facebook.com/tacos.e.rojo" target="_blank" rel="noopener noreferrer"><FaFacebookSquare/></a>
+                    </div>
+
                 </div>
-                <div className="flex justify-center text-xl">
-                    <span>Merch</span>
-                    <a className="px-1 hover:text-red-600 text-3xl" href="https://www.instagram.com/shoptacoselrojo/" target="_blank" rel="noopener noreferrer"><FaInstagramSquare/></a>
-                </div>
+                
+                
             </HomeContainer>
 
             <HomeContainer>
@@ -55,17 +90,11 @@ function Home (){
                     Events
                 </TitleContainer>
                 
-
-                <h3 className="text-left pl-1 lg:text-3xl">Date:</h3>
+                <div className="text-2xl my-10">
+                    <p>Stay tuned for future events</p>
+                </div>
             
-                <div className="flex pb-2 lg:text-2xl">
-                    <p>TBA</p>
-                    <p className="text-left pl-4 lg:pl-12">In our next event we will be having a give away of a pair of apple air pods everyone who buys tacos the day of.</p>
-                </div>
-                <div className="flex pb-2 lg:text-2xl">
-                    <p>TBA</p>
-                    <p className="text-left pl-4 lg:pl-12">New menu items including Rojos Burritos and Strawberry Horchata</p>
-                </div>
+                
 
             </HomeContainer>
 
@@ -87,6 +116,7 @@ function Home (){
                 
                 </div>
             </HomeContainer>
+
         </div>
     )
 }
